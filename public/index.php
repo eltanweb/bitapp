f89409081b30818972a57c062e29ba5a0e19e9d1
Ю. Бедарев <yu.bedarev@hsdigital.ru>
2018-10-12 17:20:30 +0700

    Added nginx & php dockerfile

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
-<?php
+#!/usr/bin/env php
+<?php declare(strict_types=1);
 
-    include_once ('../src/Application.php');
+    //  default root path
+    define('ROOT_DIR', dirname(__DIR__));
 
+    // include vendor
+    include_once ROOT_DIR . '/vendor/autoload.php';
+
+    // start application
     $app = new App();
     $app->run();
 
